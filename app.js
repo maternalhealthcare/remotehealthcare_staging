ada7d321561a1e3d45d87b40931042cdab8ee093
Deepak Ramachandran <deepak.r@quest-global.com>
2016-01-12 09:34:32 +0530

    updating application on 12/1/2016

--- app.js
+++ app.js
@@ -13,36 +13,93 @@ const FHIR_DOMAIN_URL = 'http://ec2-52-77-242-7.ap-southeast-1.compute.amazonaws
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '../../../dist/'));
 
-//var proxyUrl = 'http://' + '<username>' + ':' + '<password>' + '@' + 'cis-india-pitc-bangalorez.proxy.corporate.ge.com:' + 80;
+// var proxyUrl = 'http://' + '<username>' + ':' + '<password>' + '@' + 'cis-india-pitc-bangalorez.proxy.corporate.ge.com:' + 80;
 // var proxyUrl='https_proxy=http://http-proxy.health.ge.com:88';
 // request = request.defaults({proxy: proxyUrl});
 
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '../../../dist/index.html'));
 }
 );
 
+app.post('/api/proxyUrl', function(req, res, next) {
+    // var patientData = req.body;	
+    let url = req.headers.url;
+    console.log('url ' + url);
+    var uri = FHIR_DOMAIN_URL + url;
 
-app.get('/api/worklists', function(req, res, next) {
-    var options = '';
+    console.log('uri >>  ' + uri);
+
     var datas = JSON.stringify(req.body);
-    options = {
-        uri: FHIR_DOMAIN_URL +'patientRegistration/search',
+    var options = {
+        uri: uri,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: datas
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            next(error);
+        } else {
+            res.send(response);
+        }
+    });
+
+});
+
+
+app.get('/api/proxyUrl', function(req, res, next) {
+
+    let url = req.headers.url;
+    console.log('url ' + url);
+
+    var uri = FHIR_DOMAIN_URL + url;
+
+    var datas = JSON.stringify(req.body);
+    var options = {
+        uri: uri,
         method: 'GET',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
         },
         body: datas
     };
-    
-    
+
     request(options, function(error, response, body) {
         if (error) {
             next(error);
         }
-        if (!error && response.statusCode == 200 ) {
+        if (!error && response.statusCode == 200) {
+            res.json(JSON.parse(body));
+        }
+    });
+});
+
+
+app.get('/api/worklists', function(req, res, next) {
+    var options = '';
+    var datas = JSON.stringify(req.body);
+    options = {
+        uri: FHIR_DOMAIN_URL + 'patientRegistration/search',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: datas
+    };
+
+
+    request(options, function(error, response, body) {
+        if (error) {
+            next(error);
+        }
+        if (!error && response.statusCode == 200) {
             res.json(JSON.parse(body));
         }
     });
@@ -52,18 +109,16 @@ app.get('/api/getWorklists', function(req, res, next) {
     var options = '';
     var datas = JSON.stringify(req.body);
     var searchString = req.headers['searchstring'];
-    if(typeof req.headers['searchstring'] !== 'undefined') {
+    if (typeof req.headers['searchstring'] !== 'undefined') {
         options = {
-            uri: FHIR_DOMAIN_URL +'patientRegistration/search?searchString=' + searchString,
+            uri: FHIR_DOMAIN_URL + 'patientRegistration/search?searchString=' + searchString,
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
             },
             body: datas
         };
-    }
-    else
-    {
+    } else {
         options = {
             uri: FHIR_DOMAIN_URL + 'patientRegistration/search',
             method: 'GET',
@@ -73,125 +128,6 @@ app.get('/api/getWorklists', function(req, res, next) {
             body: datas
         };
     }
-    
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        if (!error && response.statusCode == 200) {
-            res.json(JSON.parse(body));
-        }
-    });
-});
-    
-app.post('/api/registerPatient', function(req, res, next) {
-    //var patientData = req.body;	
-    var uri = FHIR_DOMAIN_URL + 'patientRegistration/register';	
-   
-    var datas = JSON.stringify(req.body);
-    var options = {
-        uri: uri,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        else {
-            res.send(response);
-        }
-    });
-
-});
-
-app.post('/api/updatePatient', function(req, res, next) {
-    //var patientData = req.body;
-    var uri =  FHIR_DOMAIN_URL +'patientRegistration/update';			
- 
-    var datas = JSON.stringify(req.body);
-    var options = {
-        uri: uri,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        else {
-            res.send(response);
-        }
-    });
-
-});
-
-app.post('/api/saveVisit', function(req, res, next) {
-
-    var datas = JSON.stringify(req.body);
-
-    var options = {
-        uri: FHIR_DOMAIN_URL + 'patientVisit/createVisit',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        else {
-            res.send(response);
-        }
-    });
-});
-
-app.get('/api/getVisits', function(req, res, next) {
-    var datas = JSON.stringify(req.body);
-    var patientId = req.headers['patientid'];
-    var options = {
-        uri: FHIR_DOMAIN_URL + 'patientVisit/searchVisits?patientReference=' + patientId,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        if (!error && response.statusCode == 200) {
-            res.json(JSON.parse(body));
-        }
-    });
-
-});
-
-app.get('/api/getPatientDetail', function(req, res, next) {
-
-    var datas = JSON.stringify(req.body);
-    var patientId = req.headers['patientid'];
-    var options = {
-        uri: FHIR_DOMAIN_URL + 'patientRegistration/searchByID?id=' + patientId,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
 
     request(options, function(error, response, body) {
         if (error) {
@@ -203,26 +139,145 @@ app.get('/api/getPatientDetail', function(req, res, next) {
     });
 });
 
-app.get('/api/getSubCenterDetails', function(req, res, next) {
-    var datas = JSON.stringify(req.body);
-    var options = {
-        uri: FHIR_DOMAIN_URL + 'organization/searchAllSC',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        body: datas
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            next(error);
-        }
-        if (!error && response.statusCode == 200) {
-            res.json(JSON.parse(body));
-        }
-    });
-});
+// app.post('/api/registerPatient', function(req, res, next) {
+//    //var patientData = req.body;	
+//    var uri = FHIR_DOMAIN_URL + 'patientRegistration/register';	
+//   
+//    var datas = JSON.stringify(req.body);
+//    var options = {
+//        uri: uri,
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        else {
+//            res.send(response);
+//        }
+//    });
+//
+// });
+//
+// app.post('/api/updatePatient', function(req, res, next) {
+//    //var patientData = req.body;
+//    var uri =  FHIR_DOMAIN_URL +'patientRegistration/update';			
+// 
+//    var datas = JSON.stringify(req.body);
+//    var options = {
+//        uri: uri,
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        else {
+//            res.send(response);
+//        }
+//    });
+//
+// });
+//
+// app.post('/api/saveVisit', function(req, res, next) {
+//
+//    var datas = JSON.stringify(req.body);
+//
+//    var options = {
+//        uri: FHIR_DOMAIN_URL + 'patientVisit/createVisit',
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        else {
+//            res.send(response);
+//        }
+//    });
+// });
+//
+// app.get('/api/getVisits', function(req, res, next) {
+//    var datas = JSON.stringify(req.body);
+//    var patientId = req.headers['patientid'];
+//    var options = {
+//        uri: FHIR_DOMAIN_URL + 'patientVisit/searchVisits?patientReference=' + patientId,
+//        method: 'GET',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        if (!error && response.statusCode == 200) {
+//            res.json(JSON.parse(body));
+//        }
+//    });
+//
+// });
+//
+// app.get('/api/getPatientDetail', function(req, res, next) {
+//
+//    var datas = JSON.stringify(req.body);
+//    var patientId = req.headers['patientid'];
+//    var options = {
+//        uri: FHIR_DOMAIN_URL + 'patientRegistration/searchByID?id=' + patientId,
+//        method: 'GET',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        if (!error && response.statusCode == 200) {
+//            res.json(JSON.parse(body));
+//        }
+//    });
+// });
+//
+// app.get('/api/getSubCenterDetails', function(req, res, next) {
+//    var datas = JSON.stringify(req.body);
+//    var options = {
+//        uri: FHIR_DOMAIN_URL + 'organization/searchAllSC',
+//        method: 'GET',
+//        headers: {
+//            'Content-Type': 'application/json; charset=utf-8'
+//        },
+//        body: datas
+//    };
+//
+//    request(options, function(error, response, body) {
+//        if (error) {
+//            next(error);
+//        }
+//        if (!error && response.statusCode == 200) {
+//            res.json(JSON.parse(body));
+//        }
+//    });
+// });
 
 app.listen(port, function() {
     console.log('app is listening in PORT' + port);
