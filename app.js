9dec90a9a3d13f8f1d244f2810d0f57bbcc09483
Deepak Ramachandran <deepak.r@quest-global.com>
2016-01-15 19:07:32 +0530

    Updating App on 15/01/2016

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.use(express.static(__dirname + '/dist/'));
 // request = request.defaults({proxy: proxyUrl});
 
 
-//request = request.defaults({proxy: proxyUrl});
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 }
